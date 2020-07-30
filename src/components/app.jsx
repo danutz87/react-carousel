ac1a18a1d912b94b9093d7f51267f0ce1d59ae5f
daniel ghimpu <danielghimpu@yahoo.com>
2020-07-30 15:13:08 +0100

    Dependabot alerts sorted

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Carousel from './carousel';
+
+const styles = {
+  margin: 'auto',
+  width: '500px'
+};
+
+const App = () => {
+  return (
+    <div style={styles}>
+      <Carousel />
+    </div>
+  );
+};
+
+export default App;
